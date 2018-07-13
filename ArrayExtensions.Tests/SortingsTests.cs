273dc1caae2b5385ba1f6f5d30833bcb429b742b
Ronimeister <ronimeisterhs@gmail.com>
2018-07-13 23:44:40 +0300

    Project added. Need some addition in Comparators.cs

---
+++ ArrayExtensions.Tests/SortingsTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ArrayExtensions;
+
+namespace ArrayExtensions.Tests
+{
+    [TestFixture]
+    public class SortingsTests
+    {
+        [TestCase(new int[] { 35, 74, 67, 512 }, null, new int[] { 0, -6, 7 }, new int[] { int.MaxValue, -20, -41 }, new int[] {  int.MinValue, int.MaxValue })]
+        public void SortByDefault(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5)
+        {
+            int[][] actual = new int[5][];
+            actual[0] = arr1;
+            actual[1] = arr2;
+            actual[2] = arr3;
+            actual[3] = arr4;
+            actual[4] = arr5;
+
+            int[][] expected = new int[5][];
+            expected[0] = arr5;
+            expected[1] = arr3;
+            expected[2] = arr1;
+            expected[3] = arr4;
+            expected[4] = arr2;
+
+            actual.BubbleSort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 35, 74, 67, 512 }, null, new int[] { 0, -6, 7 }, new int[] { int.MaxValue, -20, -41 }, new int[] {  int.MinValue, int.MaxValue })]
+        public void SortBySumArrayRowIncComparer(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5)
+        {
+            int[][] actual = new int[5][];
+            actual[0] = arr1;
+            actual[1] = arr2;
+            actual[2] = arr3;
+            actual[3] = arr4;
+            actual[4] = arr5;
+
+            int[][] expected = new int[5][];
+            expected[0] = arr5;
+            expected[1] = arr3;
+            expected[2] = arr1;
+            expected[3] = arr4;
+            expected[4] = arr2;
+
+            actual.BubbleSort(new Comparators.SumArrayRowIncComparer());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 35, 74, 67, 512 }, null, new int[] { 0, -6, 7 }, new int[] { int.MaxValue, -20, -41 }, new int[] { int.MinValue, int.MaxValue })]
+        public void SortBySumArrayRowDecComparer(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5)
+        {
+            int[][] actual = new int[5][];
+            actual[0] = arr1;
+            actual[1] = arr2;
+            actual[2] = arr3;
+            actual[3] = arr4;
+            actual[4] = arr5;
+
+            int[][] expected = new int[5][];
+            expected[0] = arr2;
+            expected[1] = arr4;
+            expected[2] = arr1;
+            expected[3] = arr3;
+            expected[4] = arr5;
+
+            actual.BubbleSort(new Comparators.SumArrayRowDecComparer());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 35, 74, 67, 512 }, null, new int[] { 0, -6, 7 }, new int[] { int.MaxValue, -20, -41 }, new int[] { int.MinValue, int.MaxValue })]
+        public void SortByMaxValueInRowIncComparer(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5)
+        {
+            int[][] actual = new int[5][];
+            actual[0] = arr1;
+            actual[1] = arr2;
+            actual[2] = arr3;
+            actual[3] = arr4;
+            actual[4] = arr5;
+
+            int[][] expected = new int[5][];
+            expected[0] = arr3;
+            expected[1] = arr1;
+            expected[2] = arr4;
+            expected[3] = arr5;
+            expected[4] = arr2;
+
+            actual.BubbleSort(new Comparators.MaxValueInRowIncComparer());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 35, 74, 67, 512 }, null, new int[] { 0, -6, 7 }, new int[] { int.MaxValue, -20, -41 }, new int[] { int.MinValue, int.MaxValue })]
+        public void SortByMaxValueInRowDecComparer(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5)
+        {
+            int[][] actual = { arr1, arr2, arr3, arr4, arr5 };
+
+            int[][] expected = { arr2, arr4, arr5, arr1, arr3 };
+
+            actual.BubbleSort(new Comparators.MaxValueInRowDecComparer());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ArrayExtensions/Comparators.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayExtensions
+{
+    public class Comparators
+    {
+        public class SumArrayRowIncComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if (lhs == null)
+                {
+                    return 1;
+                }
+
+                if (rhs == null)
+                {
+                    return -1;
+                }
+
+                return lhs.Sum() - rhs.Sum();
+            }
+        }
+
+        public class SumArrayRowDecComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if (lhs == null)
+                {
+                    return -1;
+                }
+
+                if (rhs == null)
+                {
+                    return 1;
+                }
+
+                return rhs.Sum() - lhs.Sum();
+            }
+        }
+
+        public class MaxValueInRowIncComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if(lhs == null)
+                {
+                    return 1;
+                }
+
+                if (rhs == null)
+                {
+                    return -1;
+                }
+
+                return FindMax(lhs) - FindMax(rhs);
+            }
+
+            private int FindMax(int[] array)
+            {
+                int maxValue = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if(array[i] > maxValue)
+                    {
+                        maxValue = array[i];
+                    }
+                }
+
+                return maxValue;
+            }
+        }
+
+        public class MaxValueInRowDecComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if (lhs == null)
+                {
+                    return -1;
+                }
+
+                if (rhs == null)
+                {
+                    return 1;
+                }
+
+                return FindMax(rhs) - FindMax(lhs);
+            }
+
+            private int FindMax(int[] array)
+            {
+                int maxValue = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > maxValue)
+                    {
+                        maxValue = array[i];
+                    }
+                }
+
+                return maxValue;
+            }
+        }
+
+        public class MinValueInRowIncComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if (lhs == null)
+                {
+                    return 1;
+                }
+
+                if (rhs == null)
+                {
+                    return -1;
+                }
+
+                return FindMin(lhs) - FindMin(rhs);
+            }
+
+            private int FindMin(int[] array)
+            {
+                int minValue = int.MaxValue;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] < minValue)
+                    {
+                        minValue = array[i];
+                    }
+                }
+
+                return minValue;
+            }
+        }
+
+        public class MinValueInRowDecComparer : IComparer<int[]>
+        {
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                if (ReferenceEquals(lhs, rhs))
+                {
+                    return 0;
+                }
+
+                if (lhs == null)
+                {
+                    return -1;
+                }
+
+                if (rhs == null)
+                {
+                    return 1;
+                }
+
+                return FindMin(rhs) - FindMin(lhs);
+            }
+
+            private int FindMin(int[] array)
+            {
+                int minValue = int.MaxValue;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] < minValue)
+                    {
+                        minValue = array[i];
+                    }
+                }
+
+                return minValue;
+            }
+        }
+    }
+}
