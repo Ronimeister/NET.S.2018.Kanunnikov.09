1148af4161dedfcb4c0ceddac6a1b918bebbdbc9
Alexey Kanunnikov <31667550+Ronimeister@users.noreply.github.com>
2018-07-19 00:09:45 +0300

    Comments added

--- Bank/AccountHolder/AccountHolder.cs
+++ Bank/AccountHolder/AccountHolder.cs
@@ -1,10 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Bank
 {
+    /// <summary>
+    /// Class representing person attached to account
+    /// </summary>
     public sealed class AccountHolder : IEquatable<AccountHolder>
     {
         #region Private fields
@@ -22,6 +25,10 @@ namespace Bank
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Represent holder's name
+        /// </summary>
+        /// <exception cref="ArgumentException">Throws when setted value is equal to null, empty or started not with uppercase letter</exception>
         public string Name
         {
             get => _name;
@@ -41,6 +48,10 @@ namespace Bank
             }
         }
 
+        /// <summary>
+        /// Represent holder's surname
+        /// </summary>
+        /// <exception cref="ArgumentException">Throws when setted value is equal to null, empty or started not with uppercase letter</exception> 
         public string Surname
         {
             get => _surname;
@@ -60,6 +71,10 @@ namespace Bank
             }
         }
 
+        /// <summary>
+        /// Represent holder's contact phone
+        /// </summary>
+        /// <exception cref="ArgumentException">Throws when setted value is equal to null, empty or not in form of phone</exception> 
         public string ContactPhone
         {
             get => _contactPhone;
@@ -79,6 +94,10 @@ namespace Bank
             }
         }
 
+        /// <summary>
+        /// Represent holder's contact email
+        /// </summary>
+        /// <exception cref="ArgumentException">Throws when setted value is equal to null, empty or not in form of email</exception> 
         public string Email
         {
             get => _email;
@@ -99,6 +118,14 @@ namespace Bank
         }
         #endregion
 
+        /// <summary>
+        /// Constructor for <see cref="AccountHolder"/> class
+        /// </summary>
+        /// <param name="name">Holder's name</param>
+        /// <param name="surname">Holder's surname</param>
+        /// <param name="contactPhone">Holder's contact phone</param>
+        /// <param name="email">Holder's email</param>
+        /// <param name="holderId">Holder's ID</param>
         public AccountHolder(string name, string surname, string contactPhone, string email, long holderId)
         {
             Name = name;
@@ -110,6 +137,11 @@ namespace Bank
         }
 
         #region IEquetable and System.Object overrided methods
+        /// <summary>
+        /// Check if two <see cref="AccountHolder"/> objects are equal
+        /// </summary>
+        /// <param name="other"><see cref="AccountHolder"/> object to compare with</param>
+        /// <returns>bool value</returns>
         public bool Equals(AccountHolder other)
         {
             if (other is null)
@@ -125,6 +157,11 @@ namespace Bank
             return _holderId == other._holderId && _name == other._name && _surname == other._surname;
         }
 
+        /// <summary>
+        /// <see cref="System.Object.Equals(object)"/> overrided method that check if two objects are equal
+        /// </summary>
+        /// <param name="obj"><see cref="System.Object"/> object to compare with</param>
+        /// <returns>bool value</returns>
         public override bool Equals(object obj)
         {
             if (obj is null)
@@ -145,8 +182,16 @@ namespace Bank
             return Equals((AccountHolder) obj);
         }
 
+        /// <summary>
+        /// <see cref="System.Object.Equals(object)"/> overrided method that return hash code for this instance
+        /// </summary>
+        /// <returns>hash code for this instance</returns>
         public override int GetHashCode() => _holderId.GetHashCode() ^ 199;
 
+        /// <summary>
+        /// <see cref="System.Object.Equals(object)"/> overrided method that return string representation of this instance
+        /// </summary>
+        /// <returns>string representation of this instance</returns>
         public override string ToString()
         {
             return $"(Name: {_name} | Surname: {_surname} | Contact phone: {_contactPhone} | Email: {_email} | Id: {_holderId})";
@@ -154,16 +199,25 @@ namespace Bank
         #endregion
 
         #region Public API
+        /// <summary>
+        /// Add created account to holder account's list
+        /// </summary>
+        /// <param name="account">Current instance of holder</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="account"/> is equal to null.</exception>
         public void AddAccount(Account account)
         {
             if (account == null)
             {
-                throw new ArgumentNullException();
+                throw new ArgumentNullException($"{nameof(account)} can't be equal to null!");
             }
 
             _holderAccounts.Add(account);
         }
 
+        /// <summary>
+        /// Return <see cref="IEnumerable{Account}"/> representation of all current holder accounts
+        /// </summary>
+        /// <returns><see cref="IEnumerable{Account}"/> representation of all current holder accounts</returns>
         public IEnumerable<Account> GetAllAccount()
             => _holderAccounts.Where(x => x.Holder._holderId == _holderId);
         #endregion
